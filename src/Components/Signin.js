0cc2d966b2a7bee71e1c1a45069a0dd0d469cea5
Rasanpreet Singh <2018csb1173@iitrpr.ac.in>
2021-02-24 21:33:59 +0530

    Update Signin.js

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -45,12 +45,12 @@ class Signin extends React.Component {
 	 <article className="br3 ba shadow-2 b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">	
 	    <main className="pa4 black-80">
 	      <div className="measure">
-	       <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+	       <fieldset id="sign_up" className="ba b--white ph0 mh0">
 	         <legend className="f2 fw6 ph0 mh0">Sign In</legend>
 	         <div className="mt3">
 	          <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 	          <input 
-	            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+	            className="pa2 input-reset ba bg-white hover-bg-black hover-black w-100"
 	            type="email" 
 	            name="email-address" 
 	            id="email-address"
@@ -60,7 +60,7 @@ class Signin extends React.Component {
 	         <div className="mv3">
 	          <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
 	          <input 
-	            className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+	            className="b pa2 input-reset ba bg-white hover-bg-black hover-black w-100" 
 	            type="password" 
 	            name="password"  
 	            id="password"
@@ -71,7 +71,7 @@ class Signin extends React.Component {
 	      <div className="">
 	       <input 
 	         onClick = {this.onSubmitSignIn}
-	         className="b ph3 pv2 input-reset ba b--black bg-transparent pointer f6 dib" 
+	         className="b ph3 pv2 input-reset ba b--black bg-white pointer f6 dib" 
 	         type="submit" 
 	         value="Sign in" 
 	         />
